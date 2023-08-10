ed8b50f9025f01d64b2f63e0f82d3d64bfaacd04
ravibalebilalu <81ravikiran@gmail.com>
2023-08-10 10:39:20 +0530

    final

--- number_guessing_game.java
+++ number_guessing_game.java
@@ -1,35 +1,37 @@
-import java.util.Random;
+/*Create a guessing game where the program generates 
+a random number, and the user has to guess it.
+ Provide hints like "too high" or "too low" 
+ after each guess.*/
 
+//import random 
+import java.util.Random;
+ //import scanner
 import java.util.Scanner;
 //number generation
 public class number_guessing_game{
+    //method for random number generation
     static int number_generator(){
         Random random = new Random();
         int x = random.nextInt(5);
         return x;
     }
-    //catch the number
-    static String number_catcher(int actual,int guessed){
-         
-         if(guessed > actual){
-            return "It is smaller than this";
-        }else {
-            return "It is bigger than this";
-        }
-    }
+     
 
 
     public static void main(String[] args) {
         
       Scanner sc = new Scanner(System.in);
       
-    
+    //initialize random number generator
      int actual_number = number_generator();
+     //initialize counter to track attempts
      int attempt = 0;
-      
+      //for loop used to give 5 chance for player
     for (int i = 0;i<5;i++){
         System.out.println("Gues the number...");
-        int guessed = sc.nextInt();  
+        //ask the user to enter guessed number
+        int guessed = sc.nextInt(); 
+        //give hints and results 
         if(guessed == actual_number && attempt < 5){
             System.out.println("You got it!");
             attempt += 1;
@@ -40,20 +42,20 @@ public class number_guessing_game{
                 System.out.println("You lost!");
             break;
         }
-            System.out.println("You are guessing too hogh!");
+            System.out.println("Too high!");
         }else if (attempt < 5){
             attempt += 1;
             if(attempt == 5){
                 System.out.println("You lost!");
                 break;
             }
-            System.out.println("You are guesing too low!");
+            System.out.println("Too low!");
         }else if (attempt == 5 && (!(guessed == actual_number))){
             System.out.println("You lost!");
         }     
              
     }
-     
+     //print total attempts
     System.out.println("You took "+ attempt +" attempts!");
      
     }
