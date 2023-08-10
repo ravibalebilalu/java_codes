043113ed4ecc68707b777a719f69c659eda379c7
ravibalebilalu <81ravikiran@gmail.com>
2023-08-10 13:14:55 +0530

    final

--- character_counter.java
+++ character_counter.java
@@ -1,14 +1,18 @@
 /*Create a program that counts the frequency of each character in a given string. */
 import java.util.Scanner;
+
 public class character_counter {
     public static void main(String[] args) {
         //initialize scanner
         Scanner sc = new Scanner(System.in);
+        
         //ask user to enter a sentence
         System.out.println("Enter a sentence....");
         String sent = sc.nextLine();
+
         //create an integer array to store frequencies.Since there are 128 ASCII characters, we can create an array of size 128 to represent all possible characters.
         int [] frequency = new int [128];
+        
         //traverse the string
         for (int i = 0;i<sent.length();i++){
             //take each characters in string
